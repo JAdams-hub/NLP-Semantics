42a5a3eed05b0e2772ceab885cf7959ba2af8760
Justin Adamson <JustinMAdamson@hotmail.com>
2022-12-14 16:11:23 +0000

    First commit, adding relevant files

---
+++ semantic.py
@@ -0,0 +1,38 @@
+﻿import spacy
+
+nlp = spacy.load("en_core_web_md")
+
+tokens = nlp('cat apple monkey banana ')
+
+# Spacy will determine how similar the words are in the tokens variable
+for token1 in tokens:
+    for token2 in tokens:
+        print(token1," - ", token2, "Similarity = ", token1.similarity(token2))
+
+# The result of the above code showed how spacy was able to determine similarities based on whether words
+# were related in a way such as whether they belong in the animal group. Spacy was also able to determine the
+# behaviours of the animal with other words such as how monkeys eat bananas. This increased the similarity score
+# between the two.
+
+# -------------------------------------------------------------------------------
+print("\nSecond example\n")
+
+tokens_test2 = nlp('Human Computer Games Television ')
+
+# Spacy will determine how similar the words are in the tokens_test2 variable
+for token1 in tokens_test2:
+    for token2 in tokens_test2:
+        print(token1," - ", token2, "Similarity = ", token1.similarity(token2))
+
+# In this second example we see a low score for all of these matches, however the highest score to the human was a
+# computer. This is very interesting as a computer and a human both have the ability to learn and store
+# information. Whether Spacy recognises this is fascinating.
+
+# ----------------------------------------------------------------------
+
+# en_core_web_sm compared to en_core_web_md:
+
+# After testing both of these, I can see that en_core_web_sm seems to find less similarities between differing words
+# compared to en_core_web_md. Despite this, en_core_web_sm does seem to run/load faster than en_core_web_md, making
+# en_core_web_sm a better choice for faster results, however a reduction in accuracy would be the compromise for that.
+
---
+++ watch_next.py
@@ -0,0 +1,36 @@
+﻿import spacy
+
+nlp = spacy.load("en_core_web_md")
+
+# The movie that will be used to compare with other movies
+last_watched_movie = "Planet Hulk: Will he save their world or destroy it? When the Hulk becomes too dangerous for the"\
+                     "Earth, the Illuminati trick Hulk into a shuttle and launch him into space to a" \
+                     "planet where the Hulk can live in peace. Unfortunately, Hulk land on the" \
+                     "planet Skaar where he is sold into slavery and trained as a gladiator."
+
+nlp_last_watched_movie = nlp(last_watched_movie)
+
+# Stores the information of other movies
+movies = []
+movie_names = []
+similarity_score =[]
+
+# Reads the lines of movie.txt and appends the relevant information to the relevant lists.
+with open("movies.txt", "r") as movies_file:
+    for line in movies_file:
+        new_line = line.replace(":", "").replace("\n", "").split(" ")
+        movies.append(" ".join(new_line[2:]))
+        movie_names.append(" ".join(new_line[0:2]))
+
+
+with open("movies.txt", "r") as movies_file:
+    for sentence in movies:
+        similarities = nlp(sentence).similarity(nlp_last_watched_movie)
+        similarity_score.append(similarities)
+
+
+best_match = similarity_score.index(max(similarity_score))
+
+print(f"Since you watched {last_watched_movie[0:11]}, we think you would love to watch {movie_names[best_match]}.")
+
+
